bcc0cd2e550816f46b9788d3d657aa9d81c35879
lokey <lokeshiitm08@gmail.com>
2017-12-14 15:04:25 +0530

    Add server side apicache for 30 seconds.

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const apicache = require('apicache');
 
-const ENVIRONMENT = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
+const ENVIRONMENT = process.env.NODE_ENV
+  ? process.env.NODE_ENV.toLowerCase()
+  : 'development';
 
-
+let cache = apicache.middleware;
 
 const http = require('http').Server(app); // eslint-disable-line new-cap
 
@@ -20,22 +23,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true,
-  }),
+    extended: true
+  })
 );
 
 app.use(
   express.static(path.join(__dirname, '../public'), {
-    maxAge: 31557600000,
-  }),
+    maxAge: 31557600000
+  })
 );
 
+app.use(cache('30 seconds'));
 app.use('/ogimage', ogImageRouter);
 
 // Server Start
 if (!module.parent) {
   http.listen(app.get('port'), () =>
-  console.log('INFO: Server running on port %d in %s mode', app.get('port'), app.get('env')),
-);
+    console.log(
+      'INFO: Server running on port %d in %s mode',
+      app.get('port'),
+      app.get('env')
+    )
+  );
 }
 module.exports = app;
